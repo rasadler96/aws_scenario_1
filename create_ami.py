d007fdfb7f17bd3e91c98e68abc928aefb2705b3
Becca Sadler <beccasadler@MacBook-Air.local>
2020-05-22 12:17:55 +0100

    Addition of function to create key pair

--- create_ami.py
+++ create_ami.py
@@ -1,5 +1,6 @@
 import boto3
 import yaml
+import os
 
 config = yaml.safe_load(open("config.yml"))
  
@@ -7,8 +8,22 @@ access_key = config['aws_access_key_id']
 secret_key = config['aws_secret_access_key']
 default_region = config['aws_default_region']
 
+# Creating session (configures credentials and default region)
 session = boto3.Session(
-	aws_access_key_id=access_key
-	aws_secret_access_key=secret_key
-	aws_default_region=default_region
+	aws_access_key_id = access_key,
+	aws_secret_access_key = secret_key
 )
+
+# Function to create key pair to be used to launch instance - input = key pair name (minus the .pem), output = keypair.pem with correct permissions. 
+def create_keypair(name_of_keypair):
+	ec2 = session.resource('ec2', region_name=default_region)
+	key_file = open('%s.pem'%name_of_keypair,'w')
+	key_pair = ec2.create_key_pair(KeyName=name_of_keypair, )
+	key_pair_contents = str(key_pair.key_material)
+	key_file.write(key_pair_contents)
+	os.system('chmod 400 %s.pem'%name_of_keypair)
+
+
+
+
+
