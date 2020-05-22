e5352156def94bc5044136c2b544a532d8177a78
Becca Sadler <beccasadler@MacBook-Air.local>
2020-05-22 12:27:04 +0100

    Addition of default region to session. Also addition of .pem files to git ignore.

--- create_ami.py
+++ create_ami.py
@@ -11,7 +11,8 @@ default_region = config['aws_default_region']
 # Creating session (configures credentials and default region)
 session = boto3.Session(
 	aws_access_key_id = access_key,
-	aws_secret_access_key = secret_key
+	aws_secret_access_key = secret_key,
+	region_name = default_region
 )
 
 # Function to create key pair to be used to launch instance - input = key pair name (minus the .pem), output = keypair.pem with correct permissions. 
@@ -22,6 +23,8 @@ def create_keypair(name_of_keypair):
 	key_pair_contents = str(key_pair.key_material)
 	key_file.write(key_pair_contents)
 	os.system('chmod 400 %s.pem'%name_of_keypair)
+
+
 
 
 
