1807e5762ccfc96aa590c464654b9775cea15e7c
Becca Sadler <beccasadler@MacBook-Air.local>
2020-05-22 10:08:35 +0100

    Creation of a boto3 session

--- create_ami.py
+++ create_ami.py
@@ -6,3 +6,9 @@ config = yaml.safe_load(open("config.yml"))
 access_key = config['aws_access_key_id']
 secret_key = config['aws_secret_access_key']
 default_region = config['aws_default_region']
+
+session = boto3.Session(
+	aws_access_key_id=access_key
+	aws_secret_access_key=secret_key
+	aws_default_region=default_region
+)
