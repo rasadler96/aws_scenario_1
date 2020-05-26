772d32f8c5c10c145191ccbdde256aa9ca1ccb9f
Becca Sadler <beccasadler@MacBook-Air.local>
2020-05-26 17:37:02 +0100

    Addition of shutdown at the end of the script

--- test_bootstrap.sh
+++ test_bootstrap.sh
@@ -7,3 +7,6 @@ apt install -y git-all
 
 cd /home/ubuntu
 git clone https://github.com/Becky-Sadler/aws-ec2-pipeline.git 
+
+# Shutdown will stop the instance. This can be used to initiate next step in creating AMI 
+shutdown now -h
