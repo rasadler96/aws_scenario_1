e1f5026376289223989787736d1650299516c463
Becky-Sadler <57992319+Becky-Sadler@users.noreply.github.com>
2021-04-07 20:25:27 +0100

    Moving session creation below functions

--- run_pipeline.py
+++ run_pipeline.py
@@ -6,33 +6,6 @@ import json
 import paramiko
 import time
 
-# Config for credentials and ec2 information
-amazon_config = yaml.safe_load(open("config.yml"))
-
-amazon_details = amazon_config['amazon'] 
-access_key = amazon_details['aws_access_key_id']
-secret_key = amazon_details['aws_secret_access_key']
-default_region = amazon_details['aws_default_region']
-
-ec2_config = yaml.safe_load(open("ec2_config.yml"))
-
-ec2_details = ec2_config['ec2_information']
-ami_id = ec2_details['ami_ID']
-keypair_name = ec2_details['keypair_name']
-security_group_id = ec2_details['security_group_ID']
-role_name = ec2_details['iam_role_name']
-
-# Creating session (configures credentials and default region)
-session = boto3.Session(
-	aws_access_key_id = access_key,
-	aws_secret_access_key = secret_key,
-	region_name = default_region
-)
-
-# Creating ec2 resource and client for session
-ec2_resource = session.resource('ec2', region_name=default_region)
-ec2_client = session.client('ec2', region_name=default_region)
-
 # Function to check the state of the ami image. 
 def image_state(amiID):
 	try: 
@@ -107,6 +80,33 @@ def terminate_instance(instanceID):
 		print('Instance terminated')
 
 # Running script from here (Above are the functions)
+
+# Config for credentials and ec2 information
+amazon_config = yaml.safe_load(open("config.yml"))
+
+amazon_details = amazon_config['amazon'] 
+access_key = amazon_details['aws_access_key_id']
+secret_key = amazon_details['aws_secret_access_key']
+default_region = amazon_details['aws_default_region']
+
+ec2_config = yaml.safe_load(open("ec2_config.yml"))
+
+ec2_details = ec2_config['ec2_information']
+ami_id = ec2_details['ami_ID']
+keypair_name = ec2_details['keypair_name']
+security_group_id = ec2_details['security_group_ID']
+role_name = ec2_details['iam_role_name']
+
+# Creating session (configures credentials and default region)
+session = boto3.Session(
+	aws_access_key_id = access_key,
+	aws_secret_access_key = secret_key,
+	region_name = default_region
+)
+
+# Creating ec2 resource and client for session
+ec2_resource = session.resource('ec2', region_name=default_region)
+ec2_client = session.client('ec2', region_name=default_region)
 
 # Defining variables for instance
 instance_details = {'BlockDeviceMappings' : [
